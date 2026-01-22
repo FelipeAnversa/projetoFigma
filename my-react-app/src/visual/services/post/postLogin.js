c2371049983b722b45599dd207f272b15f675e1a
FelipeAnversa <jdanversa@gmail.com>
2026-01-22 17:53:33 -0300

    🛠 Refatorar: remover lógica de atualização de token do interceptor e integrá-la na função de login, garantindo a persistência do refresh token

--- my-react-app/src/visual/services/post/postLogin.js
+++ my-react-app/src/visual/services/post/postLogin.js
@@ -3,10 +3,25 @@ import api from '../../../apis/api';
 export async function postLogin(login, senha) {
     try {
         const response = await api.post('/api/login', {
-            "login": login,
-            "senha": senha
+            login: login,
+            senha: senha
         });
-        localStorage.setItem('token', response.data.token);
+        const { token, refreshToken } = response.data;
+        localStorage.setItem('token', token);
+        localStorage.setItem('refreshToken', refreshToken);
+        if (refreshToken) {
+            try {
+                const refreshResponse = await api.post('/api/refresh-token', {}, {
+                    headers: {
+                        Authorization: `Bearer ${refreshToken}`,
+                    },
+                });
+                const novoToken = refreshResponse.data.token;
+                localStorage.setItem('token', novoToken);
+            } catch (error) {
+                console.error('Erro ao atualizar o token:', error);
+            }
+        }
         console.log(response.data);
         return response.data;
     } catch (error) {
